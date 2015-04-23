97041315cce55769d537d2f200120053a9385a94
JulianMayorga <mayorga.julian@gmail.com>
2015-04-23 15:54:42 -0300

    Finish chapter four

---
+++ chapter-four/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Julian Mayorga on 4/22/15.
+//  Copyright (c) 2015 Julian Mayorga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ chapter-four/Hypnosister/Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Julian Mayorga on 4/22/15.
+//  Copyright (c) 2015 Julian Mayorga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
