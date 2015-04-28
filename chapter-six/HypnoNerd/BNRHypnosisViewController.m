16a69acac05a3f2aa672511f25aaad2a5fe69abf
JulianMayorga <mayorga.julian@gmail.com>
2015-04-27 20:56:48 -0400

    Finish chapter seven

--- chapter-six/HypnoNerd/BNRHypnosisViewController.m
+++ chapter-six/HypnoNerd/BNRHypnosisViewController.m
@@ -9,14 +9,70 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisViewController() <UITextFieldDelegate>
+
+@end
+
 @implementation BNRHypnosisViewController
 
 - (void)loadView {
     // Create a view
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] init];
 
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+
+    // Setting the border style on the text field will allow us to see it more clearly
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+
+    // There will be a warning on this line. We will discuss this shortly.
+    textField.delegate = self;
+
+    [backgroundView addSubview:textField];
+
     // Set it as *the* view of this view controller
     self.view = backgroundView;
+}
+
+-(void)drawHypnoticMessage:(NSString *)message {
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+
+        // Configure the label's colors and text
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+
+        // This method resizes the label, which will be relative
+        // to the text that it is displaying
+        [messageLabel sizeToFit];
+
+        // Get a random x value that fits within the hypnosis view's width
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+
+        // Get a random y value that fits within the hypnosis view's height
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+
+        // Update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin =CGPointMake(x, y);
+        messageLabel.frame = frame;
+
+        // Add the label to the hierarchy
+        [self.view addSubview:messageLabel];
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+
+    return YES;
 }
 
 - (void)viewDidLoad {
